99f3694c67d60f35e70561784981e3e7801306a9
Minh Nhat Le <151120107+mikeblocky@users.noreply.github.com>
2025-08-15 18:32:11 +0700

    handle local

--- index.js
+++ index.js
@@ -64,6 +64,11 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:4173", 
   "http://localhost:3000",
+  "http://localhost:8080",
+  "http://localhost:8000",
+  "http://127.0.0.1:5173",
+  "http://127.0.0.1:4173",
+  "http://127.0.0.1:3000",
   "https://myjournal-ht7p.vercel.app",
   "https://myjournal.vercel.app"
 ];
